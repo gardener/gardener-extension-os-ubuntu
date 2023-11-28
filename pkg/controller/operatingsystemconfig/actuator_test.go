6339f253a1f97903bf4e9f5d7dd48b3b7e0379b5
Rafael Franzke <rafael.franzke@sap.com>
2023-11-28 16:11:18 +0100

    Adapt to changes needed for `gardener-node-agent` (#99)
    * Revendor g/g to v1.84.0
    
    * Rework directory restructure
    
    In the future, the `pkg/controller/operatingsystemconfig/generator` package can be deleted entirely
    
    * Introduce OSC controller
    
    - no longer use `oscommon` package
    - prepare for new OSC contract when `UseGardenerNodeAgent` feature gate
      is enabled
    
    * Rework main entrypoint
    
    - no longer depend on `oscommon` package
    
    * Set `UseGardenerNodeAgent` option based on gardenlet feature gate

---
+++ pkg/controller/operatingsystemconfig/actuator_test.go
@@ -0,0 +1,178 @@
+// Copyright 2023 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package operatingsystemconfig_test
+
+import (
+	"context"
+
+	"github.com/gardener/gardener/extensions/pkg/controller/operatingsystemconfig"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"github.com/gardener/gardener/pkg/utils/test"
+	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	. "github.com/gardener/gardener-extension-os-ubuntu/pkg/controller/operatingsystemconfig"
+)
+
+var _ = Describe("Actuator", func() {
+	var (
+		ctx        = context.TODO()
+		log        = logr.Discard()
+		fakeClient client.Client
+		mgr        manager.Manager
+
+		osc      *extensionsv1alpha1.OperatingSystemConfig
+		actuator operatingsystemconfig.Actuator
+	)
+
+	BeforeEach(func() {
+		fakeClient = fakeclient.NewClientBuilder().Build()
+		mgr = test.FakeManager{Client: fakeClient}
+
+		osc = &extensionsv1alpha1.OperatingSystemConfig{
+			Spec: extensionsv1alpha1.OperatingSystemConfigSpec{
+				Purpose: extensionsv1alpha1.OperatingSystemConfigPurposeProvision,
+				Units:   []extensionsv1alpha1.Unit{{Name: "some-unit", Content: pointer.String("foo")}},
+				Files:   []extensionsv1alpha1.File{{Path: "/some/file", Content: extensionsv1alpha1.FileContent{Inline: &extensionsv1alpha1.FileContentInline{Data: "bar"}}}},
+			},
+		}
+	})
+
+	When("UseGardenerNodeAgent is false", func() {
+		BeforeEach(func() {
+			actuator = NewActuator(mgr, false)
+		})
+
+		Describe("#Reconcile", func() {
+			It("should not return an error", func() {
+				userData, command, unitNames, fileNames, extensionUnits, extensionFiles, err := actuator.Reconcile(ctx, log, osc)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(userData).NotTo(BeEmpty()) // legacy logic is tested in ./generator/generator_test.go
+				Expect(command).To(BeNil())
+				Expect(unitNames).To(ConsistOf("some-unit"))
+				Expect(fileNames).To(ConsistOf("/some/file"))
+				Expect(extensionUnits).To(BeEmpty())
+				Expect(extensionFiles).To(BeEmpty())
+			})
+		})
+	})
+
+	When("UseGardenerNodeAgent is true", func() {
+		BeforeEach(func() {
+			actuator = NewActuator(mgr, true)
+		})
+
+		When("purpose is 'provision'", func() {
+			expectedUserData := `#!/bin/bash
+mkdir -p /etc/cloud/cloud.cfg.d/
+cat <<EOF > /etc/cloud/cloud.cfg.d/custom-networking.cfg
+network:
+  config: disabled
+EOF
+chmod 0644 /etc/cloud/cloud.cfg.d/custom-networking.cfg
+
+mkdir -p "/some"
+
+cat << EOF | base64 -d > "/some/file"
+YmFy
+EOF
+
+
+cat << EOF | base64 -d > "/etc/systemd/system/some-unit"
+Zm9v
+EOF
+until apt-get update -qq && apt-get install --no-upgrade -qqy containerd runc docker.io socat nfs-common logrotate jq policykit-1; do sleep 1; done
+ln -s /usr/bin/docker /bin/docker
+
+if [ ! -s /etc/containerd/config.toml ]; then
+  mkdir -p /etc/containerd/
+  containerd config default > /etc/containerd/config.toml
+  chmod 0644 /etc/containerd/config.toml
+fi
+
+mkdir -p /etc/systemd/system/containerd.service.d
+cat <<EOF > /etc/systemd/system/containerd.service.d/11-exec_config.conf
+[Service]
+ExecStart=
+ExecStart=/usr/bin/containerd --config=/etc/containerd/config.toml
+EOF
+chmod 0644 /etc/systemd/system/containerd.service.d/11-exec_config.conf
+
+systemctl daemon-reload
+systemctl enable containerd && systemctl restart containerd
+systemctl enable docker && systemctl restart docker
+systemctl enable gardener-node-init && systemctl restart gardener-node-init`
+
+			Describe("#Reconcile", func() {
+				It("should not return an error", func() {
+					userData, command, unitNames, fileNames, extensionUnits, extensionFiles, err := actuator.Reconcile(ctx, log, osc)
+					Expect(err).NotTo(HaveOccurred())
+
+					Expect(string(userData)).To(Equal(expectedUserData))
+					Expect(command).To(BeNil())
+					Expect(unitNames).To(BeEmpty())
+					Expect(fileNames).To(BeEmpty())
+					Expect(extensionUnits).To(BeEmpty())
+					Expect(extensionFiles).To(BeEmpty())
+				})
+			})
+		})
+
+		When("purpose is 'reconcile'", func() {
+			BeforeEach(func() {
+				osc.Spec.Purpose = extensionsv1alpha1.OperatingSystemConfigPurposeReconcile
+			})
+
+			Describe("#Reconcile", func() {
+				It("should not return an error", func() {
+					userData, command, unitNames, fileNames, extensionUnits, extensionFiles, err := actuator.Reconcile(ctx, log, osc)
+					Expect(err).NotTo(HaveOccurred())
+
+					Expect(userData).NotTo(BeEmpty()) // legacy logic is tested in ./generator/generator_test.go
+					Expect(command).To(BeNil())
+					Expect(unitNames).To(ConsistOf("some-unit"))
+					Expect(fileNames).To(ConsistOf("/some/file"))
+					Expect(extensionUnits).To(ConsistOf(
+						extensionsv1alpha1.Unit{
+							Name: "kubelet.service",
+							DropIns: []extensionsv1alpha1.DropIn{{
+								Name: "10-configure-resolv-conf.conf",
+								Content: `[Service]
+ExecStartPre=/opt/gardener/bin/configure_kubelet_resolv_conf.sh
+`,
+							}},
+							FilePaths: []string{"/opt/gardener/bin/configure_kubelet_resolv_conf.sh"},
+						},
+					))
+					Expect(extensionFiles).To(ConsistOf(extensionsv1alpha1.File{
+						Path:        "/opt/gardener/bin/configure_kubelet_resolv_conf.sh",
+						Permissions: pointer.Int32(0755),
+						Content: extensionsv1alpha1.FileContent{Inline: &extensionsv1alpha1.FileContentInline{Data: `#!/bin/bash
+if grep -q 'resolvConf: /etc/resolv.conf' /var/lib/kubelet/config/kubelet; then
+  sed -i -e 's|resolvConf: /etc/resolv.conf|resolvConf: /run/systemd/resolve/resolv.conf|g' /var/lib/kubelet/config/kubelet;
+fi
+`}},
+					}))
+				})
+			})
+		})
+	})
+})
---
+++ pkg/controller/operatingsystemconfig/generator/generator_test.go
@@ -0,0 +1,135 @@
+// Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package generator_test
+
+import (
+	commongen "github.com/gardener/gardener/extensions/pkg/controller/operatingsystemconfig/oscommon/generator"
+	"github.com/gardener/gardener/extensions/pkg/controller/operatingsystemconfig/oscommon/generator/test"
+	"github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/gardener/gardener-extension-os-ubuntu/pkg/controller/operatingsystemconfig/generator"
+	"github.com/gardener/gardener-extension-os-ubuntu/pkg/controller/operatingsystemconfig/generator/testfiles"
+)
+
+var logger = logr.Discard()
+
+var _ = Describe("Ubuntu OS Generator Test", func() {
+	var (
+		osc *commongen.OperatingSystemConfig
+	)
+
+	BeforeEach(func() {
+		osc = &commongen.OperatingSystemConfig{
+			Object: &v1alpha1.OperatingSystemConfig{
+				Spec: v1alpha1.OperatingSystemConfigSpec{
+					Purpose: v1alpha1.OperatingSystemConfigPurposeProvision,
+				},
+			},
+
+			CRI:       &v1alpha1.CRIConfig{Name: v1alpha1.CRINameContainerD},
+			Bootstrap: true,
+		}
+	})
+
+	Describe("Conformance Tests", func() {
+		g := generator.CloudInitGenerator()
+		test.DescribeTest(generator.CloudInitGenerator(), testfiles.Files)()
+
+		It("should render correctly with Containerd enabled during Bootstrap (osc.type = provision)", func() {
+			expectedCloudInit, err := testfiles.Files.ReadFile("cloud-init-containerd-provision")
+			Expect(err).NotTo(HaveOccurred())
+			expected := string(expectedCloudInit)
+
+			osc.Units = []*commongen.Unit{
+				{
+					Name: "cloud-config-downloader.service",
+				},
+			}
+
+			cloudInit, _, err := g.Generate(logger, osc)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(cloudInit)).To(Equal(expected))
+		})
+
+		It("should render correctly with Containerd enabled but not during Bootstrap (osc.type = reconcile)", func() {
+			expectedCloudInit, err := testfiles.Files.ReadFile("cloud-init-containerd-reconcile")
+			Expect(err).NotTo(HaveOccurred())
+			expected := string(expectedCloudInit)
+
+			osc.Files = []*commongen.File{
+				{
+					Path:    "/path",
+					Content: []byte("content"),
+				},
+			}
+
+			osc.Bootstrap = false
+			osc.Object.Spec.Purpose = v1alpha1.OperatingSystemConfigPurposeReconcile
+			cloudInit, _, err := g.Generate(logger, osc)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(cloudInit)).To(Equal(expected))
+		})
+
+		It("should render correctly with drop-in units", func() {
+			expectedCloudInit, err := testfiles.Files.ReadFile("cloud-init-with-drop-in")
+			expected := string(expectedCloudInit)
+
+			Expect(err).NotTo(HaveOccurred())
+
+			content := []byte(`[Service]
+ExecStartPre=/opt/bin/init-containerd`)
+
+			osc.Bootstrap = false
+			osc.CRI = nil
+			osc.Units = []*commongen.Unit{
+				{
+					Name:    "abc.service",
+					Content: nil,
+					DropIns: []*commongen.DropIn{
+						{
+							Name:    "10-exec-start-pre-init-config.conf",
+							Content: content,
+						},
+						{
+							Name:    "12-exec-start-pre-init-config.conf",
+							Content: content,
+						},
+					},
+				},
+				{
+					Name:    "mtu-customizer.service",
+					Content: content,
+				},
+				{
+					Name:    "other.service",
+					Content: content,
+				},
+				{
+					Name: "cloud-config-downloader.service",
+				},
+			}
+
+			cloudInit, _, err := g.Generate(logger, osc)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(cloudInit)).To(Equal(expected))
+		})
+	})
+})
