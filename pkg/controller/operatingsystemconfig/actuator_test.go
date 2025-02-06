3dbc9be0f2473b7c9e95a1d693b8c5a9150a1b1f
Niclas Schad <niclas.schad@stackit.cloud>
2025-02-06 14:37:18 +0100

    move script into /opt/gardener/bin
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>

--- pkg/controller/operatingsystemconfig/actuator_test.go
+++ pkg/controller/operatingsystemconfig/actuator_test.go
@@ -200,7 +200,7 @@ Description=Oneshot service to install requested ntp client
 
 [Service]
 Type=oneshot
-ExecStart=/bin/bash /opt/bin/install-ntp.sh systemd-timesyncd
+ExecStart=/bin/bash /opt/gardener/bin/install-ntp.sh systemd-timesyncd
 
 [Install]
 WantedBy=multi-user.target
@@ -218,7 +218,7 @@ fi
 `}},
 				}))
 				Expect(extensionFiles).To(ContainElement(extensionsv1alpha1.File{
-					Path:        "/opt/bin/install-ntp.sh",
+					Path:        "/opt/gardener/bin/install-ntp.sh",
 					Content:     extensionsv1alpha1.FileContent{Inline: &extensionsv1alpha1.FileContentInline{Data: ntpInstallScript}},
 					Permissions: ptr.To[uint32](0744),
 				}))
@@ -239,7 +239,7 @@ fi
 				Expect(userData).To(BeEmpty())
 
 				Expect(extensionFiles).To(ContainElement(extensionsv1alpha1.File{
-					Path:        "/opt/bin/install-ntp.sh",
+					Path:        "/opt/gardener/bin/install-ntp.sh",
 					Content:     extensionsv1alpha1.FileContent{Inline: &extensionsv1alpha1.FileContentInline{Data: ntpInstallScript}},
 					Permissions: ptr.To[uint32](0744),
 				}))
@@ -263,7 +263,7 @@ Description=Oneshot service to install requested ntp client
 
 [Service]
 Type=oneshot
-ExecStart=/bin/bash /opt/bin/install-ntp.sh ntpd
+ExecStart=/bin/bash /opt/gardener/bin/install-ntp.sh ntpd
 
 [Install]
 WantedBy=multi-user.target
