4aae20875d65beac8491603e200dd266905dae71
Thomas Buchner <thomas.buchner@sap.com>
2026-02-04 10:59:02 +0100

    Fix various errors in code (#283)
    * add missing license headers and fix ci
    
    * fix failing tests

--- pkg/controller/config/doc.go
+++ pkg/controller/config/doc.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // Package config is a dummy package to make k8s.io/code-generator work without an internal version.
 
 // +groupName=config.ubuntu.os.extensions.gardener.cloud
